ea9732e208ca6d18972e5ba3fc511777392d247d
mintcoke123 <weetee1014@gmail.com>
2025-07-10 23:26:42 +0900

    feat: 뉴스 api 데이터 전역변수로 관리리

--- src/components/store/store.ts
+++ src/components/store/store.ts
@@ -1,11 +1,8 @@
 import { create } from 'zustand';
-
-type Theme = 'light' | 'dark';
-
-interface ThemeStore {
-  theme: Theme;
-  toggleTheme: () => void;
-}
+import { fetchNewsByCategory } from '../../api/newsApi';
+import { Article } from '../../types/article';
+import { Category } from '../../types/category';
+import { ThemeStore } from '../../types/theme';
 
 export const useThemeStore = create<ThemeStore>((set) => ({
   theme: 'light',
@@ -13,4 +10,26 @@ export const useThemeStore = create<ThemeStore>((set) => ({
     set((state) => ({
       theme: state.theme === 'light' ? 'dark' : 'light',
     })),
+}));
+
+interface NewsDataStore {
+  newsData: Article[];
+  setNewsData: (category: Category) => Promise<void>;
+  isLoading: boolean;
+}
+
+export const useNewsDataStore = create<NewsDataStore>((set) => ({
+  newsData: [],
+  isLoading: false,
+  setNewsData: async (category: Category) => {
+    set({ isLoading: true });
+    try {
+      const newsData = await fetchNewsByCategory(category);
+      set({ newsData });
+    } catch (error) {
+      console.error('뉴스 데이터 가져오기 실패', error);
+    } finally {
+      set({ isLoading: false });
+    }
+  },
 }));
