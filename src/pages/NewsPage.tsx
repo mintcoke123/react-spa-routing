681dfad0be7d6efc696e06812f59399f85b38cd2
mintcoke123 <weetee1014@gmail.com>
2025-07-10 23:28:15 +0900

    feat: 뉴스 데이터 전역변수로 관리리

--- src/pages/NewsPage.tsx
+++ src/pages/NewsPage.tsx
@@ -1,40 +1,27 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { fetchNewsByCategory } from '../api/newsApi';
-import { Article } from '../types/article';
 import { Category } from '../types/category';
 import Header from '../components/header/Header';
-import { styled } from 'styled-components';
 import NewsList from '../components/main/NewsList';
-import Loading from '../components/main/Loading';
+import { useNewsDataStore } from '../components/store/store';
 
 export default function NewsPage() {
   const { category } = useParams<{ category: Category }>();
-  const [articles, setArticles] = useState<Article[]>([]);
-  const [loading, setLoading] = useState(true);
+  const { newsData, isLoading, setNewsData } = useNewsDataStore();
 
   useEffect(() => {
     if (!category) return;
 
-    setLoading(true);
-    fetchNewsByCategory(category)
-      .then((data) => {
-        console.log(data); // ✅ 브라우저에서 확인
-        setArticles(data);
-      })
-      .catch((err) => {
-        console.error('데이터 받아오기 실패', err);
-      })
-      .finally(() => setLoading(false));
-  }, [category]);
+    setNewsData(category);
+  }, [category, setNewsData]);
 
   return (
     <main>
       <h2>{category} 뉴스</h2>
-      {loading && <p>로딩 중...</p>}
-      {!loading && <p>뉴스 개수: {articles.length}</p>}
+      {isLoading && <p>로딩 중...</p>}
+      {!isLoading && <p>뉴스 개수: {newsData.length}</p>}
       <Header />
-      {loading ? <Loading /> : <NewsList />}
+      <NewsList />
     </main>
   );
 }
